7213e69a03aa09f317aa544a8a07f626118af5a7
Ravin Chowdhury <chowavc@gmail.com>
2021-07-19 19:21:13 +0530

    Added defect angle determination script

--- utils.py
+++ utils.py
@@ -1,17 +1,63 @@
 ## Importing useful packages
 import numpy as np
+import glob
 from PIL import Image
 import matplotlib.pyplot as plt
 
 
+## Cross corellate image with a template
+def normxcorr2(template, image, mode="full"):
+	"""
+	Input arrays should be floating point numbers.
+	:param template: N-D array, of template or filter you are using for cross-correlation.
+	Must be less or equal dimensions to image.
+	Length of each dimension must be less than length of image.
+	:param image: N-D array
+	:param mode: Options, "full", "valid", "same"
+	full (Default): The output of fftconvolve is the full discrete linear convolution of the inputs. 
+	Output size will be image size + 1/2 template size in each dimension.
+	valid: The output consists only of those elements that do not rely on the zero-padding.
+	same: The output is the same size as image, centered with respect to the ‘full’ output.
+	:return: N-D array of same dimensions as image. Size depends on mode parameter.
+	"""
+
+	# If this happens, it is probably a mistake
+	if np.ndim(template) > np.ndim(image) or \
+			len([i for i in range(np.ndim(template)) if template.shape[i] > image.shape[i]]) > 0:
+		print("normxcorr2: TEMPLATE larger than IMG. Arguments may be swapped.")
+
+	template = template - np.mean(template)
+	image = image - np.mean(image)
+
+	a1 = np.ones(template.shape)
+	# Faster to flip up down and left right then use fftconvolve instead of scipy's correlate
+	ar = np.flipud(np.fliplr(template))
+	out = fftconvolve(image, ar.conj(), mode=mode)
+	
+	image = fftconvolve(np.square(image), a1, mode=mode) - \
+			np.square(fftconvolve(image, a1, mode=mode)) / (np.prod(template.shape))
+
+	# Remove small machine precision errors after subtraction
+	image[np.where(image < 0)] = 0
+
+	template = np.sum(np.square(template))
+	out = out / np.sqrt(image * template)
+
+	# Remove any divisions by 0 or very close to 0
+	out[np.where(np.logical_not(np.isfinite(out)))] = 0
+	
+	# Return cross-correlation result
+	return out
+
+
 
 ## Take in the rotation angle of a defect and create defect mask array
-def maskTemplate(phi0, ra=5, nBrushes=2, dispComp=False, dispResults=False):
+def maskTemplate(phi0, ra=5, nBrushes=2, dispComp=False, dispResults=False, saveMask=True):
 
 	# Conversion factor degrees to radians
 	dtoR = np.pi/180
 
-	# create template x+iy and modify it to  ((x+iy)/(SQRT(x^2 + y^2)))^(-4) for 4-brush patter
+	# Create template x+iy and modify it to  ((x+iy)/(SQRT(x^2 + y^2)))^(-4) for 4-brush pattern
 	txPixels = 2*ra + 1
 	tyPixels = 2*ra + 1
 
@@ -19,51 +65,54 @@ def maskTemplate(phi0, ra=5, nBrushes=2, dispComp=False, dispResults=False):
 	real_z = np.zeros((txPixels, tyPixels))
 	imag_z = np.zeros((txPixels, tyPixels))
 
-	# indexed vector [-ra, ... -1, 0, 1, ... ra]
+	# Indexed vector [-ra, ... -1, 0, 1, ... ra]
 	vRange = np.linspace(-ra, ra, num=2*ra+1)
 
 	# Replace row/column in complex number arrays
 	for i in range(2*ra + 1):
 
-		# fill rows with this vector ("x")
+		# Fill rows with this vector ("x")
 		real_z[:, i] = vRange
 
-		# fill columns with this vector ("y")
+		# Fill columns with this vector ("y")
 		imag_z[i, :] = vRange
 
-	# force center values to unity
+	# Force center values to unity
 	real_z[ra, ra] = 1
 	imag_z[ra, ra] = 1
 
-	# gives x + iy
+	# Gives x + iy
 	z0  = real_z + imag_z*1j
 
-	# x+iy rotated  by -90 degrees (~ y -ix)   (was +Re, -Im but this rotates the patern clockwise ...)
+	# x + iy rotated  by -90 degrees (y - ix)   (was +Re, -Im but this rotates the patern clockwise ...)
 	z90 = imag_z - real_z*1j
 
-	# combine the orthogonal complex fields at angle phi0
+	# Combine the orthogonal complex fields at angle phi0
 	z = z0*np.cos(phi0*dtoR) + z90*np.sin(phi0*dtoR)
 
-	# normalize  to unit vectors so have exp[i(phi - phi0)]
+	# Normalize  to unit vectors so have exp[i(phi - phi0)]
 	template = z/np.sqrt(real_z**2 + imag_z**2)
 
-	# invert to create a sharp 4-fold cross
+	# Invert to create a sharp 4-fold cross
 	# template = 1./template^4
 
-	# invert to create a sharp cross with nbrushes arms
+	# Invert to create a sharp cross with nbrushes arms
 	# jem april 2009
 	template = 1/template**nBrushes
 
-	# zero the elements outside radius ra
+	# Zero the elements outside radius ra
 	template = (np.sqrt(real_z**2 + imag_z**2) < ra)*template
 
-	# compute real and imaginary parts of the template
+	# Compute real and imaginary parts of the template
 	# (although seems none of these was passed back to the main program in the original code - jem)
-	realT = ((np.real(template) + 1)/2)*255
-	imaginaryT = ((np.imag(template) + 1)/2)*255
+	realT = (np.real(template) + 1)
+	imaginaryT = (np.imag(template) + 1)
 
 	# Direct sum of the real/imaginary parts (mean)
-	bothT = (realT + imaginaryT)/2
+	bothT = (realT + imaginaryT)
+
+	# Normalize bothT
+	bothT = ((bothT/np.amax(bothT))*255).astype(int)
 
 	# If the user wanted to display each parts
 	if dispComp:
@@ -113,14 +162,150 @@ def maskTemplate(phi0, ra=5, nBrushes=2, dispComp=False, dispResults=False):
 		# Show plot
 		plt.show()
 
-	# Create name for output image
-	maskPath = 'masks/' + str(phi0).replace('.', '_') + '.png'
+	# If user wants to save the masks
+	if saveMask:
 
-	# Numpy array to image
-	maskIm = Image.fromarray(np.uint8(bothT))
+		# Whole part of angle
+		whole = str(phi0).split('.')[0].zfill(3)
 
-	# Save mask
-	maskIm.save(maskPath)
+		# Decimal part of angle
+		decimal = str(phi0).split('.')[-1]
+
+		# Create name for output image
+		maskPath = 'masks/' + whole + '_' + decimal + '.png'
+
+		# Numpy array to image
+		maskIm = Image.fromarray(np.uint8(bothT))
+
+		# Save mask
+		maskIm.save(maskPath)
+
+
+
+## Elementwise sum for two arrays
+def dot_prod(array1, array2):
+
+	# Multiply each element
+	prod = np.multiply(array1, array2)
+
+	# Get total
+	total = np.sum(prod)
+
+	# Return result
+	return total
+
+
+
+## Find the best angle the defect correlates to
+def find_angle(defectZoom):
+
+	# Where masks are stored
+	maskPaths = glob.glob('masks/*.png')
+
+	# Storing totals
+	tots = []
+
+	# Storing angles
+	angles = []
+
+	# Go through every mask
+	for i, maskPath in enumerate(maskPaths):
+
+		# String describing angle
+		anStr = maskPath.split('\\')[-1].split('.')[0]
+
+		# Find angle and add to array
+		angles.append(float(anStr.replace('_', '.')))
+
+		# Read mask template
+		template = np.asarray(Image.open(maskPath))
+
+		# Find elementwise sum
+		tots.append(dot_prod(defectZoom, template))
+
+	# Convert totals list to numpy array
+	tots = np.asarray(tots)
+
+	# Index of largest sum
+	index = np.where(tots == np.amax(tots))[0][0]
+
+	# Get best angle from list
+	bestAngle = angles[index]
+
+	# # Showing defect and selected template
+	# plt.imshow(defectZoom, cmap='gray')
+	# plt.show()
+	# plt.imshow(Image.open(maskPaths[index]), cmap='gray')
+	# plt.show()
+
+	# # Showing plot of totals vs angles
+	# plt.plot(angles, tots)
+	# plt.scatter(bestAngle, np.amax(tots), color='r')
+	# plt.show()
+
+	# Return result
+	return bestAngle
+
+
+
+## Find defect angles in an image
+def image_angles(imPath):
+
+	print('\nDetecting defect angles in ' + imPath)
+
+	# Get path to label file corresponding to image
+	labelPath = imPath.replace('images', 'labels').replace('tif', 'txt')
+
+	# Create path to output label file
+	outPath = labelPath.replace('input', 'output')
+
+	# Read the image as numpy array
+	image = np.asarray(Image.open(imPath))
+
+	# Read labels as numpy array
+	labelMat = np.loadtxt(labelPath)
+
+	# Output matrix
+	outMat = []
+
+	# Get semi width and length of mask
+	testMask = np.asarray(Image.open(glob.glob('masks/*.png')[0]))
+	sw = (len(testMask) - 1)//2
+
+	# For every angle in the defect
+	for i, defect in enumerate(labelMat):
+
+		# Find center of detected defect
+		xc = int(defect[1]*len(image[0]))
+		yc = int(defect[2]*len(image))
+
+		# Slice out the defect
+		defArray = image[yc - sw:yc + sw+1, xc - sw:xc + sw+1]
+
+		# Get mean value
+		mean = np.mean(defArray)
+
+		# Increase contrast
+		defArray = (defArray - mean)*100 + mean
+
+		# Get the best defect angle
+		bestAngle = find_angle(defArray)
+
+		# Output matrix line
+		line = np.copy(defect)
+
+		# Now add the angle
+		line = np.append(line, bestAngle)
+
+		# Add this to the output array
+		outMat.append(line)
+
+	# Convert outarray to numpy array
+	outMat = np.asarray(outMat)
+
+	# Save output matrix as txt
+	np.savetxt(outPath, outMat)
+
 
 
 if __name__ == '__main__':
